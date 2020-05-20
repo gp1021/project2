a13daa95a15c9a86f6a4d210c7a6cb17e162d6e9
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-20 16:47:43 -0400

    Update LabPosition.java

--- The Riddler's Labyrinth Lab/src/riddlersLabyrinth/LabPosition.java
+++ The Riddler's Labyrinth Lab/src/riddlersLabyrinth/LabPosition.java
@@ -1,12 +1,15 @@
 package riddlersLabyrinth;
 
 public class LabPosition {
+	//instance variables
 	private int _r;
 	private int _c;
+	//constructor for a position class
 	public LabPosition(int row, int col) {
 		_r = row;
 		_c = col;
 	}
+	//getters
 	public int getRow() { return _r; }
 	public int getCol() { return _c; }
 	
