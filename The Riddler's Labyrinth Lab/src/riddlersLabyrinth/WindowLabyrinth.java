b118c12d2c8a192e754440f7a7061f56687d3e98
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-20 19:58:27 -0400

    Update WindowLabyrinth.java

--- The Riddler's Labyrinth Lab/src/riddlersLabyrinth/WindowLabyrinth.java
+++ The Riddler's Labyrinth Lab/src/riddlersLabyrinth/WindowLabyrinth.java
@@ -6,34 +6,31 @@ public class WindowLabyrinth {
 	
 	
 	public void msg(ImageIcon icon) {
-		JOptionPane.showMessageDialog(null, icon); //sends a message to the user
+		JOptionPane.showMessageDialog(null, icon); //sends an image message to the user
 	}
 	
 	public String in(String msg) {
-		return JOptionPane.showInputDialog(msg);
+		return JOptionPane.showInputDialog(msg); //asks the user to input a response
 	}
 	
 	public void println(String msg) {
-		System.out.println(msg);
+		System.out.println(msg); //prints a message
 	}
 	
+	//shows the selected buttons as well as the labyrinth
 	public int option1(String[] options, String map) {
 		return JOptionPane.showOptionDialog(null, map, "Select your direction.", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
 	}
 	
 	public void msg1(String s) {
-		JOptionPane.showMessageDialog(null, s);
+		JOptionPane.showMessageDialog(null, s); //shows the user a jpane message
 		
 	}
 
 	public void msg2(String[][] strings) {
-		JOptionPane.showMessageDialog(null, strings);
+		JOptionPane.showMessageDialog(null, strings); //shows a jpane message in the form of a 2d string
 		
 	}
 
-	public int option(String[] replay, String string) {
-		// TODO Auto-generated method stub
-		return JOptionPane.showOptionDialog(null, string, "Replay", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, replay, replay[0]);
-	}
 
 }
